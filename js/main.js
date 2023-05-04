a2c44d97c7c3361851be3b6628e8d0867210eb12
RudiJr <rudi.juniior@gmail.com>
2023-05-04 17:41:44 -0300

    Created the carousel with swiper Js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ for (let i = 0; i < menuBtn.length; i++) {
     this.className += " active";
   });
 }
+
+// swiper project section
+const swiper = new Swiper('.swiper', {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
