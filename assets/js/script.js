043dea44313c7fc3ee44bd27e3b13b3de43a794f
RudiJr <rudi.juniior@gmail.com>
2023-05-16 16:29:35 -0300

    Created scroll reveal for the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,16 @@ navIcon.onclick = () => {
 navIcon.classList.remove('bx-x');
 nav.classList.remove('active');
 
+// scroll reveal 
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
